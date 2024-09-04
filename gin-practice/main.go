c3c0351f1e87f843f18b824f9ca0d317d0e5d347
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-09-04 21:28:57 +0530

    custom http configuration in gin

--- gin-practice/main.go
+++ gin-practice/main.go
@@ -1,50 +1,21 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Person struct {
-	Name    string `form:"name" json:"name"`
-	Address string `form:"address" json:"address"`
-}
-
 func main() {
-	route := gin.Default()
-	route.LoadHTMLFiles("form.html") // Load the HTML file
-	route.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "form.html", nil)
-	})
-	route.POST("/submit", getName)
-	route.GET("/testing", startPage)
-	route.Run(":8085")
-}
+	router := gin.Default()
 
-func getName(c *gin.Context) {
-	var person Person
-	if c.ShouldBind(&person) == nil {
-		log.Println("-----binding html-------")
-		log.Println("name : " + person.Name)
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
-	c.String(http.StatusOK, "success")
-}
-
-func startPage(c *gin.Context) {
-	var person Person
-	if c.BindQuery(&person) == nil {
-		log.Println("====== Bind By Query String ======")
-		log.Println(person.Name)
-		log.Println(person.Address)
-	}
-
-	if c.BindJSON(&person) == nil {
-		log.Println("====== Bind By JSON ======")
-		log.Println(person.Name)
-		log.Println(person.Address)
-	}
-
-	c.String(http.StatusOK, "Success")
+	s.ListenAndServe()
 }
